63eff27c4fec2248014aa5c103076badcb8880aa
sam <wax654@gmail.com>
2021-08-25 12:09:33 -0700

    Same Freq done

--- same-frequency.js
+++ same-frequency.js
@@ -1,3 +1,20 @@
 // add whatever parameters you deem necessary
-function sameFrequency() {
+function sameFrequency(num1, num2) {
+    const num1Freq = freqCount(num1 + '');
+    const num2Freq = freqCount(num2 + '');
+    if(num1Freq.size !== num2Freq.size) return false;
+    for(let num of num1Freq.keys()){
+        if(num1Freq.get(num) !== num2Freq.get(num)) return false;
+    }
+    return true;
+}
+
+function freqCount(str) {
+    const result = new Map();
+    for (let letter of str) {
+        const occurances = result.get(letter);
+        if(occurances) result.set(letter, occurances + 1);
+        else result.set(letter, 1);
+    }
+    return result;
 }
