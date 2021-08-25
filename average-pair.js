14323f0049d1e2dedfa0d2e4a2fe1727dae9d21f
sam <wax654@gmail.com>
2021-08-25 11:54:43 -0700

    average pair and two array object done

--- average-pair.js
+++ average-pair.js
@@ -1,3 +1,15 @@
 // add whatever parameters you deem necessary
-function averagePair() {
+function averagePair(arr, target) {
+    let leftIdx = 0;
+    let rightIdx = arr.length - 1;
+    while(leftIdx < rightIdx) {
+        const num1 = arr[leftIdx];
+        const num2 = arr[rightIdx];
+        const avg = (num1 + num2)/2;
+        if(avg === target) return true;
+        else if(avg < target) leftIdx++;
+        else                    rightIdx--;
+    }
+    return false;
+
 }
--- two-array-object.js
+++ two-array-object.js
@@ -1,3 +1,8 @@
 // add whatever parameters you deem necessary
-function twoArrayObject() {
+function twoArrayObject(keys, vals) {
+    return keys.reduce((res, key, i) => {
+        const val = i < vals.length ? vals[i] : null;
+        res[key] = val;
+        return res;
+    }, {});    
 }
