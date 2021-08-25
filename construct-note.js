6e497c10b5c68c3f2c6562bc1828eeed1fa913c5
sam <wax654@gmail.com>
2021-08-25 11:39:45 -0700

    Construct Note Finished

---
+++ construct-note.js
@@ -0,0 +1,18 @@
+// add whatever parameters you deem necessary
+function constructNote(note, letters) {
+    const noteLetterFreq = freqCount(note);
+    const lettersFreq = freqCount(letters);
+    for(let letter in noteLetterFreq) {
+        if(!lettersFreq[letter]) return false;
+        if(lettersFreq[letter] < noteLetterFreq[letter]) return false;
+    }
+    return true;
+}
+
+function freqCount(str) {
+    const result = {};
+    for(let letter of str) {
+        result[letter] ? result[letter]++ : result[letter] = 1;
+    }
+    return result;
+}
